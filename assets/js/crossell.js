2ff6083008ef2a915530e9a2be763a51d5657c19
jack maynard <jack.ui.maynard@gmail.com>
2021-12-19 21:58:20 +0000

    update on files

--- assets/js/crossell.js
+++ assets/js/crossell.js
@@ -1,1 +1,25 @@
 // Crosssell.js
+/*$("button").on('click', function() {
+    $.getJSON( "courses.json", function(obj) {
+        $.each(obj, function(key, value) {
+           $("ul").append("<li>"+value.ThumbnailUrl+"</li>"); 
+        });
+    } );
+});
+*/
+$.ajax({
+    url: 'http://localhost:5500/assets/js/courses.json', 
+    type: 'GET',
+    success: function(data){
+        $.each(data, function(key, value) {
+            $("div").append("<img src="+value.ThumbnailUrl+" />" +
+            "<div>"+value.Id+"</div>" +  // course id
+            "<div class='course-title'>"+value.Title+"</div>" + 
+            "<div class='course-info'>"+value.Description+"</div>" +
+            "<div class='info-link'>" +value.InfoUrl+"</div>" +
+            "<button>Add to Cart</button><button>More Info</button>"
+        );
+        console.log(data);
+    });
+    },
+});
