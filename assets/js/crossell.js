f162420054dd6ffef9c0839254ec122e8a94f91f
jack maynard <jack.ui.maynard@gmail.com>
2021-12-20 01:14:39 +0000

    update

--- assets/js/crossell.js
+++ assets/js/crossell.js
@@ -1,13 +1,6 @@
 // Crosssell.js
-/*$("button").on('click', function() {
-    $.getJSON( "courses.json", function(obj) {
-        $.each(obj, function(key, value) {
-           $("ul").append("<li>"+value.ThumbnailUrl+"</li>"); 
-        });
-    } );
-});
-*/
-$.ajax({
+
+/* $.ajax({
     url: 'http://localhost:5500/assets/js/courses.json', 
     type: 'GET',
     success: function(data){
@@ -22,4 +15,29 @@ $.ajax({
         console.log(data);
     });
     },
-});
+}); */
+
+
+$.ajax({
+    url: 'http://localhost:5500/assets/js/courses.json', 
+    type: 'GET',
+    success: function(data){
+        var course_data = '';
+        $.each(data, function(key, value){
+            course_data += '<div class="item">'
+            course_data += '<img src='+value.ThumbnailUrl+ ' />';
+            course_data += '<div class="title">'+value.Title+'</div>';
+            course_data += '<div class="description">'+value.Description+'</div>';
+            course_data += '<div class="info-url">'+value.InfoUrl+'</div>';
+            course_data += '<button class="basket">'+ 'Add to Basket' +'</button>';
+            course_data += '<button class="more-info">'+ 'More Info' +'</button>';
+            course_data += '</div>';
+        });
+        $('#course').append(course_data);
+    }
+})
+
+// to dos
+// add event listeners 
+// create a modal
+// add ore js to interrogate that json for cross sell
